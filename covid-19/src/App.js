61f1ca172d28e21f5b97fc43031f552f293efd5b
jason <jason_doan94@yahoo.com>
2020-06-05 18:59:34 -0400

    cards and axios

--- covid-19/src/App.js
+++ covid-19/src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <CardDeck>
+        <Card bg="secondary" text="white" style={{ margin: "10px" }}>
+          <Card.Body>
+            <Card.Title>Cases</Card.Title>
+            <Card.Text>100</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card
+          bg="danger"
+          text={"white"}
+          className="text-center"
+          style={{ margin: "10px" }}
         >
-          Learn React
-        </a>
-      </header>
+          <Card.Body>
+            <Card.Title>Deaths</Card.Title>
+            <Card.Text>0</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card
+          bg="success"
+          text={"white"}
+          className="text-center"
+          style={{ margin: "10px" }}
+        >
+          <Card.Body>
+            <Card.Title>Recovered</Card.Title>
+            <Card.Text>99</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
     </div>
   );
 }
